77bc2caed9214f0ab600b27b47d21965b52442a8
Jon DeCamp <jon.decamp@oracle.com>
2020-06-17 10:50:12 -0700

    Bastion subnet and security list

--- modules/services/vcn/main.tf
+++ modules/services/vcn/main.tf
@@ -33,6 +33,17 @@ resource "oci_core_subnet" "web_subnet" {
   dhcp_options_id     = oci_core_vcn.primary_vcn.default_dhcp_options_id
 }
 
+resource "oci_core_subnet" "bastion_subnet" {
+  cidr_block          = "10.1.3.0/24"
+  display_name        = "BastionSubnet"
+  dns_label           = "bastionsubnet"
+  security_list_ids   = [oci_core_security_list.bastion_security_list.id]
+  compartment_id      = var.compartment_ocid
+  vcn_id              = oci_core_vcn.primary_vcn.id
+  route_table_id      = oci_core_vcn.primary_vcn.default_route_table_id
+  dhcp_options_id     = oci_core_vcn.primary_vcn.default_dhcp_options_id
+}
+
 output "web_subnet_id" {
   value = oci_core_subnet.web_subnet.id
   description = "Web subnet ID"
