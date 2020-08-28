fb57cc13fec73215219577bbdba808d7b3dcdb8a
Jon DeCamp <jon.decamp@oracle.com>
2020-08-28 13:43:13 -0700

    remove comments in lb main.tf

--- modules/app_tier/load_balancer/main.tf
+++ modules/app_tier/load_balancer/main.tf
@@ -158,65 +158,10 @@ resource "tls_locally_signed_cert" "lb_cert" {
   ]
 }
 
-
-# resource "tls_private_key" "example" {
-#   algorithm   = "ECDSA"
-#   ecdsa_curve = "P384"
-# }
-
-# resource "tls_self_signed_cert" "load_balancer_cert" {
-#   key_algorithm   = "ECDSA"
-#   # private_key_pem = file(var.load_balancer_cert_key)
-#   private_key_pem = tls_private_key.example.private_key_pem
-#   is_ca_certificate = true
-
-#   subject {
-#     common_name  = "example.com"
-#     organization = "ACME Examples, Inc"
-#   }
-
-#   validity_period_hours = 12
-
-#   allowed_uses = [
-#     "key_encipherment",
-#     "digital_signature",
-#     "server_auth",
-#     "client_auth",
-#     "cert_signing"
-#   ]
-# }
-
-# resource "oci_load_balancer_certificate" "delete_me" {
-#   #Required
-#   certificate_name = "test_cert_name"
-#   load_balancer_id = oci_load_balancer.dmz_load_balancer.id
-
-#   #Optional
-#   #ca_certificate = "${var.certificate_ca_certificate}"
-#   # passphrase = "${var.certificate_passphrase}"
-#   # private_key = tls_private_key.example.private_key_pem
-#   # public_certificate = tls_self_signed_cert.load_balancer_cert.cert_pem
-#   # ca_certificate = tls_self_signed_cert.load_balancer_cert.cert_pem
-#   private_key = tls_private_key.lb_private_key.private_key_pem
-#   public_certificate = tls_locally_signed_cert.lb_cert.cert_pem
-#   ca_certificate = tls_self_signed_cert.ca.cert_pem
-
-#   # lifecycle {
-#   #     create_before_destroy = true
-#   # }
-# }
-
 resource "oci_load_balancer_certificate" "lb_cert" {
-  #Required
   certificate_name = "lb_cert"
   load_balancer_id = oci_load_balancer.dmz_load_balancer.id
 
-  #Optional
-  #ca_certificate = "${var.certificate_ca_certificate}"
-  # passphrase = "${var.certificate_passphrase}"
-  # private_key = tls_private_key.example.private_key_pem
-  # public_certificate = tls_self_signed_cert.load_balancer_cert.cert_pem
-  # ca_certificate = tls_self_signed_cert.load_balancer_cert.cert_pem
   private_key = tls_private_key.lb_private_key.private_key_pem
   public_certificate = tls_locally_signed_cert.lb_cert.cert_pem
   ca_certificate = tls_self_signed_cert.ca.cert_pem
@@ -225,18 +170,6 @@ resource "oci_load_balancer_certificate" "lb_cert" {
   #     create_before_destroy = true
   # }
 }
-
-# resource "oci_load_balancer_listener" "lb_app_listener" {
-#   load_balancer_id         = oci_load_balancer.app_load_balancer.id
-#   name                     = "https"
-#   default_backend_set_name = oci_load_balancer_backend_set.lb-app-bes.name
-#   port                     = var.app_lb_port
-#   protocol                 = "HTTP"
-
-#   ssl_configuration {
-#     certificate_name = oci_load_balancer_certificate.test_certificate.certificate_name
-#   }
-# }
 
 resource "oci_load_balancer_listener" "lb_dmz_listener" {
   load_balancer_id         = oci_load_balancer.dmz_load_balancer.id
