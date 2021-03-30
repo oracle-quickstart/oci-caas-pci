ecb6549cdec314a4c44e3ffb7315e27804099952
sskarnaw <sakshi.karnawat@oracle.com>
2021-03-30 09:39:43 -0700

    Adding error handling blocks

--- admin-scripts/app_vault.py
+++ admin-scripts/app_vault.py
@@ -55,77 +55,87 @@ def validatePassword(password):
     if caps and lower and num and specialChar and length:
         return 'valid'
     else:
-        print("A password must contain at least 12 characters, a number, a special character, a lowercase letter and a uppercase letter!!")
+        print(
+            "A password must contain at least 12 characters, a number, a special character, a lowercase letter and a uppercase letter!!")
         return 'not valid'
 
 
 # This function creates a new vault in the given compartment using KMS Vault client and waits until the vault
 # status is Active
 def createVault(compartment_id, ident, config):
-    key_management_client = oci.key_management.KmsVaultClient(config)
-    sac_composite = oci.key_management.KmsVaultClientCompositeOperations(key_management_client)
-    print(" Creating vault oci-caas-{} in {} compartment.".format(ident,
-                                                                  compartment_id))
+    try:
+        key_management_client = oci.key_management.KmsVaultClient(config)
+        sac_composite = oci.key_management.KmsVaultClientCompositeOperations(key_management_client)
+        print(" Creating vault oci-caas-{} in {} compartment.".format(ident,
+                                                                      compartment_id))
 
-    vault_details = oci.key_management.models.CreateVaultDetails(
-        compartment_id=compartment_id,
-        vault_type="DEFAULT",
-        display_name="oci-caas-" + ident)
+        vault_details = oci.key_management.models.CreateVaultDetails(
+            compartment_id=compartment_id,
+            vault_type="DEFAULT",
+            display_name="oci-caas-" + ident)
 
-    print("Vault details {}.".format(vault_details.vault_type))
-    print("Waiting for vault to be created - This could take a few minutes.")
+        print("Vault details {}.".format(vault_details.vault_type))
+        print("Waiting for vault to be created - This could take a few minutes.")
 
-    # Create vault and wait for the vault to become Active
-    response = sac_composite.create_vault_and_wait_for_state(
-        vault_details,
-        wait_for_states=[oci.key_management.models.Vault.LIFECYCLE_STATE_ACTIVE])
-    return response
+        # Create vault and wait for the vault to become Active
+        response = sac_composite.create_vault_and_wait_for_state(
+            vault_details,
+            wait_for_states=[oci.key_management.models.Vault.LIFECYCLE_STATE_ACTIVE])
+        return response
+    except:
+        print("Error with vault creation. Exiting.")
 
 
 # This function creates a new management key in the given vault using KMS Management client and waits until the key
 # is Enabled
 def createKey(key_name, compartment_id, config, service_endpoint):
-    key_management_client = oci.key_management.KmsManagementClient(config, service_endpoint)
-    key_mgmt_composite = oci.key_management.KmsManagementClientCompositeOperations(key_management_client)
-    print(" Creating key {} in compartment {}.".format(key_name, compartment_id))
+    try:
+        key_management_client = oci.key_management.KmsManagementClient(config, service_endpoint)
+        key_mgmt_composite = oci.key_management.KmsManagementClientCompositeOperations(key_management_client)
+        print(" Creating key {} in compartment {}.".format(key_name, compartment_id))
 
-    key_shape = oci.key_management.models.KeyShape(algorithm="AES", length=32)
-    key_details = oci.key_management.models.CreateKeyDetails(
-        compartment_id=compartment_id,
-        display_name=key_name,
-        key_shape=key_shape)
+        key_shape = oci.key_management.models.KeyShape(algorithm="AES", length=32)
+        key_details = oci.key_management.models.CreateKeyDetails(
+            compartment_id=compartment_id,
+            display_name=key_name,
+            key_shape=key_shape)
 
-    # Create management key and wait for the key to be Enabled
-    response = key_mgmt_composite.create_key_and_wait_for_state(key_details,
-                                                                wait_for_states=[
-                                                                    oci.key_management.models.Key.LIFECYCLE_STATE_ENABLED])
-    return response
+        # Create management key and wait for the key to be Enabled
+        response = key_mgmt_composite.create_key_and_wait_for_state(key_details,
+                                                                    wait_for_states=[
+                                                                        oci.key_management.models.Key.LIFECYCLE_STATE_ENABLED])
+        return response
+    except:
+        print("Error with vault creation. Exiting.")
 
 
 def create_secret(compartment_id, secret_content, secret_name, vault_id, key_id, config):
-    vaults_client = oci.vault.VaultsClient(config)
-    vaults_client_composite = oci.vault.VaultsClientCompositeOperations(vaults_client)
-    print("Creating a secret {}.".format(secret_name))
+    try:
+        vaults_client = oci.vault.VaultsClient(config)
+        vaults_client_composite = oci.vault.VaultsClientCompositeOperations(vaults_client)
+        print("Creating a secret {}.".format(secret_name))
 
-    # Create secret_content_details that needs to be passed when creating secret.
-    secret_description = "This is just a test"
-    secret_content_details = oci.vault.models.Base64SecretContentDetails(
-        content_type=oci.vault.models.SecretContentDetails.CONTENT_TYPE_BASE64,
-        name=secret_content,
-        stage="CURRENT",
-        content=secret_content)
-    secrets_details = oci.vault.models.CreateSecretDetails(compartment_id=compartment_id,
-                                                           description=secret_description,
-                                                           secret_content=secret_content_details,
-                                                           secret_name=secret_name,
-                                                           vault_id=vault_id,
-                                                           key_id=key_id)
+        # Create secret_content_details that needs to be passed when creating secret.
+        secret_description = "This is just a test"
+        secret_content_details = oci.vault.models.Base64SecretContentDetails(
+            content_type=oci.vault.models.SecretContentDetails.CONTENT_TYPE_BASE64,
+            name=secret_content,
+            stage="CURRENT",
+            content=secret_content)
+        secrets_details = oci.vault.models.CreateSecretDetails(compartment_id=compartment_id,
+                                                               description=secret_description,
+                                                               secret_content=secret_content_details,
+                                                               secret_name=secret_name,
+                                                               vault_id=vault_id,
+                                                               key_id=key_id)
 
-    # Create secret and wait for the secret to become active
-    response = vaults_client_composite.create_secret_and_wait_for_state(create_secret_details=secrets_details,
-                                                                        wait_for_states=[
-                                                                            oci.vault.models.Secret.LIFECYCLE_STATE_ACTIVE])
-    return response
+        # Create secret and wait for the secret to become active
+        response = vaults_client_composite.create_secret_and_wait_for_state(create_secret_details=secrets_details,
+                                                                            wait_for_states=[
+                                                                                oci.vault.models.Secret.LIFECYCLE_STATE_ACTIVE])
+        return response
+    except:
+        print("Error with vault creation. Exiting.")
 
 
 def get_vault(client, vault_id):
@@ -139,7 +149,7 @@ def get_key(key_id, service_endpoint, config):
 
 # Reads the compartment ID and the identity from the configuration file
 def get_comaprtmentID_and_Ident():
-    filename = from_file(file_location="~/.oci-caas/oci-caas-pci.conf")
+    filename = "~/.oci-caas/oci-caas-pci.conf"
     # Open the file in read mode
     with open(filename, 'r') as file_object:
         i = 0
@@ -161,7 +171,7 @@ def get_comaprtmentID_and_Ident():
 
 # Writes the vault ID and the management key ID to the configuration file
 def write_VaultID_and_KeyID(vault_id, key_id):
-    filename = from_file(file_location="~/.oci-caas/oci-caas-pci.conf")
+    filename = "~/.oci-caas/oci-caas-pci.conf"
     # Open the file in read mode
     with open(filename, 'a') as file_object:
         file_object.write(" \nvault_id={} \n".format(vault_id))
@@ -169,7 +179,6 @@ def write_VaultID_and_KeyID(vault_id, key_id):
 
 
 if __name__ == "__main__":
-
     stripe_api_sk, stripe_api_pk, ecom_db_pw = getKeys()
 
     compartment_id, ident = get_comaprtmentID_and_Ident()
@@ -180,13 +189,21 @@ if __name__ == "__main__":
     KEY_NAME = "mgmt-key"
 
     vault = createVault(COMPARTMENT_ID, VAULT_NAME, configuration).data
-    VAULT_ID = vault.id
+    try:
+        VAULT_ID = vault.id
+    except:
+        print("Unable to retrieve oci vault. Exiting due to errors.")
 
     print(" Created vault {} with id : {}".format(VAULT_NAME, VAULT_ID))
-
-    service_endpoint = vault.management_endpoint
+    try:
+        service_endpoint = vault.management_endpoint
+    except:
+        print("Unable to retrieve management endpoint. Exiting due to errors.")
     key = createKey(KEY_NAME, COMPARTMENT_ID, configuration, service_endpoint).data
-    KEY_ID = key.id
+    try:
+        KEY_ID = key.id
+    except:
+        print("Unable to retrieve vault management key. Exiting due to errors.")
 
     print(" Created key {} with id : {}".format(KEY_NAME, KEY_ID))
 
@@ -199,9 +216,4 @@ if __name__ == "__main__":
     create_secret(COMPARTMENT_ID, ecom_db_pw, secret_name3, VAULT_ID, KEY_ID, configuration)
     print("Successfully Uploaded the secrets")
 
-    write_VaultID_and_KeyID(VAULT_ID,KEY_ID)
-
-
-
-
-
+    write_VaultID_and_KeyID(VAULT_ID, KEY_ID)
