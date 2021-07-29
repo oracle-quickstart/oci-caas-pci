90a89ba68065c9563a3749b619126cb3ced70540
sskarnaw <sakshi.karnawat@oracle.com>
2021-07-29 14:12:47 -0700

    Adding default CA to SSL certificate script

--- admin-scripts/ssl_certificate.py
+++ admin-scripts/ssl_certificate.py
@@ -68,13 +68,12 @@ def get_secret_data(path):
 
     return str(secret_data)
 
-
 # Get validation records from registration command
 def get_acme_validation_records(acme, domain_name):
     try:
         text_values = []
         response = subprocess.getoutput(
-            "acme_cmd={}; domain={}; $acme_cmd/acme.sh --issue  -d $domain --dns -d \*.$domain --yes-I-know-dns-manual-mode-enough-go-ahead-please".format(
+            "acme_cmd={}; domain={}; $acme_cmd/acme.sh --set-default-ca --server letsencrypt; $acme_cmd/acme.sh --issue  -d $domain --dns -d \*.$domain --yes-I-know-dns-manual-mode-enough-go-ahead-please".format(
                 acme, domain_name))
         print(response)
         for line in response.splitlines():
